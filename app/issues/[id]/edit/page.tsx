604dfeb84b167f8f167dfdfcfbf1340a1fe93438
ryan6t4 <mdrayhanh585@gmail.com>
2025-09-03 11:21:12 +0600

    creating-repo

---
+++ app/issues/[id]/edit/page.tsx
@@ -0,0 +1,5 @@
+const EditIssuePage = () => {
+  return 'Edit Issue Page'
+}
+
+export default EditIssuePage
---
+++ app/issues/new/page.tsx
@@ -0,0 +1,5 @@
+const NewIssuePage = () => {
+  return 'Edit Issue Page'
+}
+
+export default NewIssuePage
